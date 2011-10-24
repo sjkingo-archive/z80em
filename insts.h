bdb37cf2fc23c7bc490e3f2ef73031d5d9d92a57
Sam Kingston <sam@sjkwi.com.au>
2011-10-25 09:09:26 +1000

    added remainder of load immediate opcodes

--- insts.h
+++ insts.h
@@ -9,8 +9,13 @@ struct z80_instruction {
 
 enum z80_opcodes {
     OP_NOP=0x0,
-    OP_LD_B_N=0x6,
+    OP_LD_A_N=0x3e,
+    OP_LD_B_N=0x06,
+    OP_LD_C_N=0x0e,
+    OP_LD_D_N=0x16,
+    OP_LD_E_N=0x1e,
     OP_LD_H_N=0x26,
+    OP_LD_L_N=0x2e,
 };
 
 struct z80_instruction insts[] = {
@@ -18,8 +23,13 @@ struct z80_instruction insts[] = {
     { OP_NOP, "nop", 1 },
 
     /* load immediates */
+    { OP_LD_A_N, "ld A,n", 2 },
     { OP_LD_B_N, "ld B,n", 2 },
+    { OP_LD_C_N, "ld C,n", 2 },
+    { OP_LD_D_N, "ld D,n", 2 },
+    { OP_LD_E_N, "ld E,n", 2 },
     { OP_LD_H_N, "ld H,n", 2 },
+    { OP_LD_L_N, "ld L,n", 2 },
 
     /* always leave this last */
     { 0x0, NULL, 0 },
--- machine.c
+++ machine.c
@@ -20,20 +20,41 @@ void run_machine(unsigned char *ops, unsigned int max_pc) {
         }
         printfv("  %s\n", inst->name);
 
+        /* the meat of the emulator - instruction emulation */
         switch (opcode) {
             case OP_NOP:
                 break;
 
+            /* load immediates */
+            case OP_LD_A_N:
             case OP_LD_B_N:
-            case OP_LD_H_N: {
-                /* immediate load n into reg X */
+            case OP_LD_C_N:
+            case OP_LD_D_N:
+            case OP_LD_E_N:
+            case OP_LD_H_N:
+            case OP_LD_L_N: {
                 unsigned char n = ops[cpu->regs.pc+1];
                 switch (opcode) {
+                    case OP_LD_A_N:
+                        set_reg(REG_A, n);
+                        break;
                     case OP_LD_B_N:
                         set_reg(REG_B, n);
                         break;
+                    case OP_LD_C_N:
+                        set_reg(REG_C, n);
+                        break;
+                    case OP_LD_D_N:
+                        set_reg(REG_D, n);
+                        break;
+                    case OP_LD_E_N:
+                        set_reg(REG_E, n);
+                        break;
                     case OP_LD_H_N:
                         set_reg(REG_H, n);
+                        break;
+                    case OP_LD_L_N:
+                        set_reg(REG_L, n);
                         break;
                 }
                 break;
@@ -42,6 +63,7 @@ void run_machine(unsigned char *ops, unsigned int max_pc) {
             default:
                 panic("unhandled opcode %s (0x%x)\n", inst->name, opcode);
         }
+
         cpu->regs.pc += inst->cycles;
     }
 
